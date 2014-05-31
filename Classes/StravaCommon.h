ccc896691cf904dc661eb2c941af3652f11eff87
Sebastien Windal <sebastien@windal.net>
2014-05-31 18:02:00 -0400

    documenting some enums.

--- Classes/StravaCommon.h
+++ Classes/StravaCommon.h
@@ -9,16 +9,31 @@
 #import <Foundation/Foundation.h>
 #import <Mantle/Mantle.h>
 
+///
+/// Level of details a response object contains.
+///
+/// Strava doc: http://strava.github.io/api/#decoration
+///
+///
 typedef NS_ENUM(NSInteger, kResourceState) {
+    /// unspecified/unknown level of details
     kResourceStateUnknown = 0,
+    /// minimal level of details (most likely only the ID is valid in the object)
     kResourceStateMeta,
+    /// summary information
     kResourceStateSummary,
+    /// full details, all fields in the response object should have been set
     kResourceStateDetailed
 };
 
 
+///
+/// Strava doc: http://strava.github.io/api/v3/activities/
+///
 typedef NS_ENUM(NSInteger, kActivityType) {
+    /// unspecified/unknown
     kActivityTypeUnknown = 0,
+    /// Any kind of cycling, road, mountain, etc...
     kActivityTypeRide,
     kActivityTypeRun,
     kActivityTypeHike,
