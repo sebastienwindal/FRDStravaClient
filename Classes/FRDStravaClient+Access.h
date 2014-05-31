5635a428fd13b1e611c7a34b6982230229fb0a74
Sebastien Windal <sebastien@windal.net>
2014-05-31 15:53:52 -0400

    more docs reformatting…

--- Classes/FRDStravaClient+Access.h
+++ Classes/FRDStravaClient+Access.h
@@ -7,6 +7,21 @@
 
 #import "FRDStravaClient.h"
 
+/**
+
+The Access category provides methods to authorize your app with the Strava Rest service.
+ 
+## OAuth Flow
+
+The typical authorization flow should look like this:
+
+1. iOS app calls `authorizeCallbackURL:stateInfo:`.
+2. Upon success your app is launched back by Safari. In your AppDelegate `application:handleOpenURL:`, call `parseStravaAuthCallback:withSuccess:failure:` to parse the callback URL.
+3. In the success block of `parseStravaAuthCallback:withSuccess:failure:` call `exchangeTokenForCode:success:failure:`
+4. Upon success you are now good to go for this session... The `accessToken` in the shared instance of FRDStravaClient was automatically set and all REST calls issued by the FRDStravaClient sharedInstance should be sent with the required token. You may also want at this point to save the access token somewhere safely in your app persistent storage (like the keychain), for future run of your app, to be able to bypass that whole painful OAuth dance. Call the `setAccessToken:` method of the `[FRDStravaClient sharedInstance]` with your saved access token...
+ 
+ */
+
 @interface FRDStravaClient (Access)
 
 /**
@@ -14,18 +29,8 @@ Initiate a OAUTH authorization with Strava. This method will launch Safari at th
  
 After the user successfuly authorized your app, Safari will open your app by launching the passed `callbackUrl`, you can use `parseStravaAuthCallback:withSuccess:failure` to parse the callback data, including the authorization code required to get an access token to complete the Oauth Flow.
  
-## OAuth Flow
- 
-The typical initial authorization flow should look like this:
- 
-* iOS app calls `authorizeCallbackURL:stateInfo:`.
-* upon success your app is launched back by Safari. In your AppDelegate `application:handleOpenURL:`, call `parseStravaAuthCallback:withSuccess:failure:` to parse the callback URL.
-* In the success block of `parseStravaAuthCallback:withSuccess:failure:` call `exchangeTokenForCode:success:failure:`
-* Upon success you are now good to go for this session... The `accessToken` in the shared instance of FRDStravaClient was automatically set and all REST calls issued by the FRDStravaClient sharedInstance should be sent with the required token. You may also want at this point to save the access token somewhere safely in your app persistent storage (like the keychain), for future run of your app, to be able to bypass that whole painful OAuth dance. Call the `setAccessToken:` method of the `[FRDStravaClient sharedInstance]` with your saved access token...
- 
 @param callbackUrl The URL that will be called from Safari upon successful Authentication with the strava OAuth authorization web page. Typically you want your app to be launched back, so it should consist of a URL with a scheme you registered e.g. myAppRegisteredURLScheme://mydomain.com.
 @param stateInfo (optional) a NSString that will be passed back in the callback URL, as a state attribute (...&state=blah).
-
  */
 -(void) authorizeWithCallbackURL:(NSURL *)callbackUrl stateInfo:(NSString *)stateInfo;
 
